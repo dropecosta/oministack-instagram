c4d7de762bd934cefcb307edd6dac1177b13768b
pedro.reis@altran.com.br <pedro.reis@altran.com.br>
2019-06-23 18:44:34 -0300

    creating and logging on insomnia route for a post

--- backend/src/models/Post.js
+++ backend/src/models/Post.js
@@ -1,11 +1,11 @@
 const mongose = require('mongoose');
 
 const PostSchema = new mongose.Schema({
+    image: String,
     author: String,
     place: String,
     description: String,
     hashtags: String,
-    image: String,
     likes: {
         type: Number,
         default: 0,
