e2be30292019b237d3ae70c20019dcbd84aec25c
pedro.reis@altran.com.br <pedro.reis@altran.com.br>
2019-06-23 16:56:21 -0300

    adding mongoose and establishing comunication with mongoDb

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://dropecosta:soeuquesei@cluster0-ax0q4.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+})
 
 app.listen(3333);
 
